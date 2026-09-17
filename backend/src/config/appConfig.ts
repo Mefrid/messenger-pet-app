c4ac3b6adcb620c94b860e3102932a156b7b9f9c
Mefrid <oficialbag@gmail.com>
2026-09-17 16:07:47 +0500

    feat: Docker configuration

--- backend/src/config/appConfig.ts
+++ backend/src/config/appConfig.ts
@@ -1,17 +1,17 @@
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config({ path: `.env.${process.env?.["NODE_ENV"]}` });
 
 export const config = {
-  port: Number(process.env?.['PORT']),
-  allowedHost: process.env?.['ALLOWED_HOST'],
-  accessTokenSecret: process.env?.['JWT_ACCESS_SECRET']!,
-  refreshTokenSecret: process.env?.['JWT_REFRESH_SECRET']!,
+  port: Number(process.env?.["PORT"]),
+  allowedHost: process.env?.["ALLOWED_HOST"],
+  accessTokenSecret: process.env?.["JWT_ACCESS_SECRET"]!,
+  refreshTokenSecret: process.env?.["JWT_REFRESH_SECRET"]!,
   database: {
-    host: process.env?.['DB_HOST'],
-    port: Number(process.env?.['DB_PORT']),
-    user: process.env?.['DB_USER'],
-    password: process.env?.['DB_PASSWORD'],
-    database: process.env?.['DB_NAME'],
+    host: process.env?.["DB_HOST"],
+    port: Number(process.env?.["DB_PORT"]),
+    user: process.env?.["DB_USER"],
+    password: process.env?.["DB_PASSWORD"],
+    database: process.env?.["DB_NAME"],
   },
-}
+};
